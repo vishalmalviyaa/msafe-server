87f55fc16ad794180153e51b21bab91313b3b250
Vishal Malviya <vishalmalviyaa680@gmail.com>
2026-03-14 13:41:52 +0530

    Expose APK download endpoint

--- vishkey_backend/urls.py
+++ vishkey_backend/urls.py
@@ -2,21 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from manager.views import download_agent
 
 urlpatterns = [
 
     path('', lambda request: HttpResponse("✅ VishKey Backend Running")),
 
+    # APK DOWNLOAD
+    path('download/msafe-agent.apk', download_agent),
+
     path('admin/', admin.site.urls),
 
     # JWT auth
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/', TokenObtainPairView.as_view()),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view()),
 
     # Apps
     path('api/owner/', include('owner.urls')),
     path('api/manager/', include('manager.urls')),
     path('api/', include('users.urls')),
-
     path('api/system/', include('scalability_core.urls')),
 ]
