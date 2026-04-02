cdaaffea31436403d54fbbb43f14d4d4808ac0e0
Vishal Malviya <vishalmalviyaa680@gmail.com>
2026-04-02 14:55:25 +0530

    fix permission issue

--- manager/serializers.py
+++ manager/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import ManagerProfile
+
+User = get_user_model()
 
 
 class ManagerProfileSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=False, allow_blank=True)
+
     keys_remaining = serializers.SerializerMethodField()
 
     class Meta:
         model = ManagerProfile
         fields = [
             "id",
+            "username",
+            "password",
+            "email",
             "phone",
             "photo",
             "default_lock_message",
@@ -17,7 +28,31 @@ class ManagerProfileSerializer(serializers.ModelSerializer):
             "used_keys",
             "keys_remaining",
         ]
-        read_only_fields = ["total_keys", "used_keys", "keys_remaining"]
+
+        read_only_fields = [
+            "total_keys",
+            "used_keys",
+            "keys_remaining",
+        ]
 
     def get_keys_remaining(self, obj):
         return obj.keys_remaining()
+
+    def create(self, validated_data):
+
+        username = validated_data.pop("username")
+        password = validated_data.pop("password")
+        email = validated_data.pop("email", "")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+        )
+
+        manager = ManagerProfile.objects.create(
+            user=user,
+            **validated_data
+        )
+
+        return manager
